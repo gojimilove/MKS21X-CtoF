6459f78401cbf5847ba2e85f780eda0df95bc527
Jessica Kim <jkim00@stuy.edu>
2018-09-20 01:24:07 -0400

    working code converting CtoF and FtoC

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,1 +1,13 @@
+public class CtoFTester {
+	public static double CelsiusToFahrenheit (double Celsius) {
+		return Celsius * (9/5) + 32;
+	}
+	public static double FahrenheitToCelsius (double Fahrenheit) {
+		return (Fahrenheit - 32) / (9/5);
+	}
+	public static void main(String[] args){
+		System.out.println(CelsiusToFahrenheit(50));
+		System.out.println(FahrenheitToCelsius(50));
+	}
+}
 
